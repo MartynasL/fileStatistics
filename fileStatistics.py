1294e80783e753d9cb1e36943911567d0dace517
MartynasL <martynas.lycius@gmail.com>
2014-03-22 18:43:20 +0200

    Added function get_file_words_count which returns dictionary of words as keys and value is how many time this word occurs in files passed as *args

--- fileStatistics.py
+++ fileStatistics.py
@@ -6,5 +6,29 @@ import string
 def get_dir_filepaths(dir_path):
     filepaths = os.listdir(dir_path)
     for file in filepaths:
-        filepaths[filepaths.index(file)] = string.join([dir_path, '/', file], '')
+        filepaths[filepaths.index(file)] = string.join([dir_path,
+                                                        '/', file], '')
     return filepaths
+
+
+def get_file_words_count(*args):
+    word_count = {};
+
+    for file in args:
+        opened_file = open(file, 'r')
+
+        for line in opened_file:
+            for word in string.split(line):
+                if word_count.has_key(word):
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
+
+        opened_file.close()
+    return word_count
+
+
+
+
+
+
